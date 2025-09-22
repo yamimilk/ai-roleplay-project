d0eff27520f6d3d2071a7313e2184d6b41c1fffa
yamiya <157779936+yamimilk@users.noreply.github.com>
2025-09-22 15:07:28 +0800

    初始提交: 添加后端和前端基础结构

---
+++ backend/src/main/java/cdu/zjy/topictwo/TopicTwoApplication.java
@@ -0,0 +1,13 @@
+package cdu.zjy.topictwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicTwoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cdu/zjy/topictwo/TopicTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.zjy.topictwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
